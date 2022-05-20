72450b532601fceeccf5e75174ead90bff7e2aec
ZaniInc <stridestoreholding@gmail.com>
2022-05-20 18:36:40 +0300

    init done

---
+++ contracts/Kidnft.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./ManNft.sol";
+import "./WomanNft.sol";
+
+contract KidNft is ManNft , WomanNft { 
+
+}
